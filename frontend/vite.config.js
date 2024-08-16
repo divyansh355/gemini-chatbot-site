c1c06742fdaddf3908f8768e13f6d10d821afdbd
Divyansh <145423867+divyansh355@users.noreply.github.com>
2024-08-16 20:16:49 +0000

    vite configuration updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/frontend',
   build: {
-    outDir: 'dist',
+    outDir: '/frontend/dist',
   },
 })
